fbcab82c45a48fd11f55234a9175f7455a10a182
Victor Huicochea <vhuicochea@miners.utep.edu>
2020-02-27 18:37:50 -0800

    Header

--- lab2.h
+++ lab2.h
@@ -1,4 +1,7 @@
-void init_shell();
-void printWorkingDir();
-void getInput();
-void printWorkingDirContents();
+// Function declarations for builtin shell commands:
+int sh_cd(char **args);
+int sh_help(char **args); // Take out
+int sh_exit(char **args);
+int sh_chprmpt(char **args);
+void printDir();
+int parsePipe(char* line, char** linePiped);
