d3a2b70d16c4e0dec28b7d4ee0f42a2a942cc939
lagutierrez13 <lagutierrez13@miners.utep.edu>
2020-02-28 14:18:16 -0800

    WIP: Update file descriptions

--- builtIn.c
+++ builtIn.c
@@ -2,7 +2,11 @@
 * Author: Luis A. Gutierrez and Victor Huicochea 
 * CS 4375
 * Date: Feb 28, 2020
-* This program is a custom shell for the Unix system. This program mimics the some of the behaviors of the bash shell. 
+* This program is a custom shell for the Unix system. This program mimics some of the behaviors of the bash shell.
+* Our shell prints a prompt string specified by the by shell variable PS1 when expecting a command. The default 
+* prompt is '$'but can be changed with the command 'chprmpr. Our shell accepts the standard unix command shell syntax
+* for specifying commands and parameters. Our shell handles expected user error by using printing statements. Our
+* shell provides the follwowing built-in commands: cd, exit, chprmpt. Our shell supports the use of simple pipes. 
 */
 
 // Global Variable for prompt
